0baac5231c3f8e483affeb04858172748035e532
hpratt <hpratt111@live.com>
2020-10-17 10:22:29 -0400

    make some parameters required

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -19,10 +19,10 @@ def main():
     subparsers = parser.add_subparsers()
 
     core = subparsers.add_parser("core", help = "core model, without extended metadata or cell-type-level features")
-    core.add_argument("--rDHSs", type = str, help = "path to rDHS regions to which features correspond")
+    core.add_argument("--rDHSs", type = str, help = "path to rDHS regions to which features correspond", required = True)
     core.add_argument("--feature-files", type = str, nargs = '+', help = "path to feature matrices in JSON format")
-    core.add_argument("--sequence-matrix", type = str, help = "path to one-hot sequence matrix for rDHSs")
-    core.add_argument("--signal-z-scores", type = str, help = "path to BED file containing TF Z-scores for each rDHS")
+    core.add_argument("--sequence-matrix", type = str, help = "path to one-hot sequence matrix for rDHSs", required = True)
+    core.add_argument("--signal-z-scores", type = str, help = "path to BED file containing TF Z-scores for each rDHS", required = True)
     core.add_argument("--output-directory", type = str, help = "path to output directory")
     core.set_defaults(func = run_train_core)
 
