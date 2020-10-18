8b53f08acc14c5146bed16576dae2d75c25af221
Henry Pratt <hpratt111@live.com>
2020-10-18 01:47:41 -0400

    fix output transform

--- src/source/generator.py
+++ src/source/generator.py
@@ -21,7 +21,7 @@ def generator(sequenceMatrixReader, featureMatrixReaders, zscoreJson, rDHSs, chr
             [ numpy.array([ featureMatrixReader.read(i, lambda x: x[::-1]) for i in indexes ]) for featureMatrixReader in featureMatrixReaders ],
             numpy.array([ sequenceMatrixReader.read(i, lambda x: x[::-1]) for i in indexes ])
         )
-        outputs = [ numpy.array([ math.tanh(scores[i] / 2) ]) for i in indexes ]
+        outputs = [ numpy.array([ (math.tanh(scores[i] / 2) + 1) / 2 ]) for i in indexes ]
         pointer += batch_size
         if pointer + batch_size >= len(allIndexes):
             pointer = 0
